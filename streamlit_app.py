8ad04de7e878bdf400245cddd77e83411afa720b
R14502 <143222730+R14502@users.noreply.github.com>
2023-08-25 22:40:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
 streamlit.title('My Parent new healthy dinner')
+
 streamlit.header('Breakfast menu')
 streamlit.text('Omega3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket SMoothie')
