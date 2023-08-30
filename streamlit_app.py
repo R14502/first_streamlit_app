de4399b32213cd04ab78bd0dfff05f6ff440d0da
R14502 <143222730+R14502@users.noreply.github.com>
2023-08-30 15:13:06 +0200

    Update streamlit_app.py
    Variabilisation

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,11 @@ streamlit.dataframe(fruits_to_show)
 
 # Nouvelle section pour ajouter des renseignements sur les fruits
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # On prend la version json et on la normalise 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
