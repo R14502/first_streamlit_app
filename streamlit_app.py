1d2b8afb09ea940aba7b097b39057e73fa0ced3d
R14502 <143222730+R14502@users.noreply.github.com>
2023-08-30 15:05:16 +0200

    Update streamlit_app.py
    Affichage sous forme de tableau

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# On prend la version json et on la normalise 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# On affiche sous forme de tableau la réponse
+streamlit.dataframe(fruityvice_normalized)
